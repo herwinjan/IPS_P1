b2b6056b432c66d7b34fc666f673310b71bd443c
Herwin Jan Steehouwer <herwin@steehouwer.nu>
2019-03-03 09:12:37 +0100

    debug

--- P1Network/module.php
+++ P1Network/module.php
@@ -112,14 +112,14 @@ class P1Module extends IPSModule {
    $Data = $Data . $dt;
    IPS_LogMessage("P1Data", $Data);
 
-   preg_match('/^(1-0:1\.8\.1\((\d+\.\d+)\*kWh\))/', $Data, $output_array);
+   preg_match_all('/^(1-0:1\.8\.1\((\d+\.\d+)\*kWh\))/m', $Data, $output_array, PREG_SET_ORDER, 0);
    IPS_LogMessage("P1Data", json_encode($output_array));
-   preg_match('/^(1-0:1\.8\.2\((\d+\.\d+)\*kWh\))/', $Data, $output_array);
-   preg_match('/^(2-0:1\.8\.1\((\d+\.\d+)\*kWh\))/', $Data, $output_array);
-   preg_match('/^(2-0:1\.8\.2\((\d+\.\d+)\*kWh\))/', $Data, $output_array);
-   preg_match('/^(0-0:96\.14\.0\((\d+)\))/', $Data, $output_array);
-   preg_match('/^(1-0:1\.7\.0\((\d+.\d+)\*kW\))/', $Data, $output_array);
-   preg_match('/^(1-0:2\.7\.0\((\d+.\d+)\*kW\))/', $Data, $output_array);
+   prpreg_match_alleg_match('/^(1-0:1\.8\.2\((\d+\.\d+)\*kWh\))/m', $Data, $output_array, PREG_SET_ORDER, 0);
+   preg_match_all('/^(2-0:1\.8\.1\((\d+\.\d+)\*kWh\))/m', $Data, $output_array, PREG_SET_ORDER, 0);
+   preg_match_all('/^(2-0:1\.8\.2\((\d+\.\d+)\*kWh\))/m', $Data, $output_array, PREG_SET_ORDER, 0);
+   preg_match_all('/^(0-0:96\.14\.0\((\d+)\))/m', $Data, $output_array, PREG_SET_ORDER, 0);
+   preg_match_all('/^(1-0:1\.7\.0\((\d+.\d+)\*kW\))/m', $Data, $output_array, PREG_SET_ORDER, 0);
+   preg_match_all('/^(1-0:2\.7\.0\((\d+.\d+)\*kW\))/m', $Data, $output_array, PREG_SET_ORDER, 0);
 
    $this->SetBuffer("Data", "");
 
