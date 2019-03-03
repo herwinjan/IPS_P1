3bd8e153a0a5374a77d0162e82a037383458e310
Herwin Jan Steehouwer <herwin@steehouwer.nu>
2019-03-03 10:11:38 +0100

    debug

--- P1Network/module.php
+++ P1Network/module.php
@@ -13,6 +13,52 @@
  */
 class P1Module extends IPSModule {
 
+ private function __CreateVariable($Name, $Type, $Value, $Ident = '', $ParentID = 0) {
+  //echo "CreateVariable: ( $Name, $Type, $Value, $Ident, $ParentID ) \n";
+  if ('' != $Ident) {
+   $VarID = @IPS_GetObjectIDByIdent($Ident, $ParentID);
+   if (false !== $VarID) {
+    $this->SetVariable($VarID, $Type, $Value);
+    return $VarID;
+   }
+  }
+  $VarID = @IPS_GetObjectIDByName($Name, $ParentID);
+  if (false !== $VarID) { // exists?
+   $Obj = IPS_GetObject($VarID);
+   if (2 == $Obj['ObjectType']) { // is variable?
+    $Var = IPS_GetVariable($VarID);
+    if ($Type == $Var['VariableValue']['ValueType']) {
+     $this->SetVariable($VarID, $Type, $Value);
+     return $VarID;
+    }
+   }
+  }
+  $VarID = IPS_CreateVariable($Type);
+  IPS_SetParent($VarID, $ParentID);
+  IPS_SetName($VarID, $Name);
+  if ('' != $Ident) {
+   IPS_SetIdent($VarID, $Ident);
+  }
+  $this->SetVariable($VarID, $Type, $Value);
+  return $VarID;
+ }
+
+ private function __SetVariable($VarID, $Type, $Value) {
+  switch ($Type) {
+  case 0: // boolean
+   SetValueBoolean($VarID, $Value);
+   break;
+  case 1: // integer
+   SetValueInteger($VarID, $Value);
+   break;
+  case 2: // float
+   SetValueFloat($VarID, $Value);
+   break;
+  case 3: // string
+   SetValueString($VarID, $Value);
+   break;
+  }
+ }
  /**
   * Interne Funktion des SDK.
   *
@@ -45,11 +91,11 @@ class P1Module extends IPSModule {
    IPS_SetVariableProfileAssociation("P1TariefProfile", 1, "Nacht tarief", "", -1);
   }
 
-  $id = $this->CreateVariable("Verbuik Nacht", 2, 0, "P1VerbruikNacht", $this->InstanceID);
+  $id = $this->__CreateVariable("Verbuik Nacht", 2, 0, "P1VerbruikNacht", $this->InstanceID);
   IPS_SetVariableCustomProfile($id, "P1kWhProfile");
-  $id = $this->CreateVariable("Verbuik Dag", 2, 0, "P1VerbruikDag", $this->InstanceID);
+  $id = $this->__CreateVariable("Verbuik Dag", 2, 0, "P1VerbruikDag", $this->InstanceID);
   IPS_SetVariableCustomProfile($id, "P1kWhProfile");
-  $id = $this->CreateVariable("Huidig Vebruik", 2, 0, "P1HuidigVerbruik", $this->InstanceID);
+  $id = $this->__CreateVariable("Huidig Vebruik", 2, 0, "P1HuidigVerbruik", $this->InstanceID);
   IPS_SetVariableCustomProfile($id, "P1kWattProfile");
 
  }
@@ -190,77 +236,6 @@ class P1Module extends IPSModule {
   }
 
   return true;
- }
-
- private function __CreateCategory($Name, $Ident = '', $ParentID = 0) {
-  $RootCategoryID = $this->InstanceID;
-  echo "CreateCategory: ( $Name, $Ident, $ParentID ) \n";
-  if ('' != $Ident) {
-   $CatID = @IPS_GetObjectIDByIdent($Ident, $ParentID);
-   if (false !== $CatID) {
-    $Obj = IPS_GetObject($CatID);
-    if (0 == $Obj['ObjectType']) { // is category?
-     return $CatID;
-    }
-   }
-  }
-  $CatID = IPS_CreateCategory();
-  IPS_SetName($CatID, $Name);
-  IPS_SetIdent($CatID, $Ident);
-  if (0 == $ParentID) {
-   if (IPS_ObjectExists($RootCategoryID)) {
-    $ParentID = $RootCategoryID;
-   }
-  }
-  IPS_SetParent($CatID, $ParentID);
-  return $CatID;
- }
-
- private function __CreateVariable($Name, $Type, $Value, $Ident = '', $ParentID = 0) {
-  //echo "CreateVariable: ( $Name, $Type, $Value, $Ident, $ParentID ) \n";
-  if ('' != $Ident) {
-   $VarID = @IPS_GetObjectIDByIdent($Ident, $ParentID);
-   if (false !== $VarID) {
-    $this->SetVariable($VarID, $Type, $Value);
-    return $VarID;
-   }
-  }
-  $VarID = @IPS_GetObjectIDByName($Name, $ParentID);
-  if (false !== $VarID) { // exists?
-   $Obj = IPS_GetObject($VarID);
-   if (2 == $Obj['ObjectType']) { // is variable?
-    $Var = IPS_GetVariable($VarID);
-    if ($Type == $Var['VariableValue']['ValueType']) {
-     $this->SetVariable($VarID, $Type, $Value);
-     return $VarID;
-    }
-   }
-  }
-  $VarID = IPS_CreateVariable($Type);
-  IPS_SetParent($VarID, $ParentID);
-  IPS_SetName($VarID, $Name);
-  if ('' != $Ident) {
-   IPS_SetIdent($VarID, $Ident);
-  }
-  $this->SetVariable($VarID, $Type, $Value);
-  return $VarID;
- }
-
- private function __SetVariable($VarID, $Type, $Value) {
-  switch ($Type) {
-  case 0: // boolean
-   SetValueBoolean($VarID, $Value);
-   break;
-  case 1: // integer
-   SetValueInteger($VarID, $Value);
-   break;
-  case 2: // float
-   SetValueFloat($VarID, $Value);
-   break;
-  case 3: // string
-   SetValueString($VarID, $Value);
-   break;
-  }
  }
 
 }
